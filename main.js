fa59ed8bc9282eb8ee51d07d734ff2a82f630a80
Guneet2911 <98748570+Guneet2911@users.noreply.github.com>
2022-09-05 17:50:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ camera = document.getElementById("camera");
 
 Webcam.attach("#camera");
 
-function take_Snapshot()
+function take_snapshot()
 {
 
     Webcam.snap(function(data_uri){
@@ -19,9 +19,28 @@ function take_Snapshot()
 
 console.log("ml5 version:" , ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/6K9DcKSdm/model.json' , modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/yB7MQUKxp/model.json' , modelLoaded);
 
 function modelLoaded()
 {
     console.log("Model Loaded!");
+}
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img , gotResult);
+}
+
+function gotResult(error , results)
+{
+    if(error)
+    {
+     console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
